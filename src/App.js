c6046c66aa070bc296ceacfdadfab94f3d3f1bb2
lhnaseeb63 <lhnaseeb@gmail.com>
2024-03-08 17:40:53 +0300

    aws-amplify v6 split the storage module into 3 functions

--- src/App.js
+++ src/App.js
@@ -11,12 +11,15 @@ import {
   TextField,
   View,
   withAuthenticator,
+  Image
 } from "@aws-amplify/ui-react";
 import { listNotes } from "./graphql/queries";
 import {
   createNote as createNoteMutation,
   deleteNote as deleteNoteMutation,
 } from "./graphql/mutations";
+import { uploadData, getUrl, remove } from 'aws-amplify/storage';
+
 import { generateClient } from 'aws-amplify/api';
 
 const client = generateClient();
@@ -31,16 +34,28 @@ const App = ({ signOut }) => {
   async function fetchNotes() {
     const apiData = await client.graphql({ query: listNotes });
     const notesFromAPI = apiData.data.listNotes.items;
+    await Promise.all(
+      notesFromAPI.map(async (note) => {
+        if (note.image) {
+          const url = await getUrl({ key: note.name });
+          note.image = url.url;
+        }
+        return note;
+      })
+    );
     setNotes(notesFromAPI);
   }
 
   async function createNote(event) {
     event.preventDefault();
     const form = new FormData(event.target);
+    const image = form.get("image");
     const data = {
       name: form.get("name"),
       description: form.get("description"),
+      image: image.name,
     };
+    if (!!data.image) await uploadData({ key: data.name, data: image });
     await client.graphql({
       query: createNoteMutation,
       variables: { input: data },
@@ -49,9 +64,10 @@ const App = ({ signOut }) => {
     event.target.reset();
   }
 
-  async function deleteNote({ id }) {
+  async function deleteNote({ id, name }) {
     const newNotes = notes.filter((note) => note.id !== id);
     setNotes(newNotes);
+    await remove({ key: name });
     await client.graphql({
       query: deleteNoteMutation,
       variables: { input: { id } },
