77853eec8b62aed6a96d896707598ddb8cf4040d
lhnaseeb63 <lhnaseeb@gmail.com>
2024-03-08 19:15:29 +0300

    ugh

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "pleasework": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
