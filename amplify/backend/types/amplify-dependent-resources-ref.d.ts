e0a0214a6f6cb2bee88dd3866d23402c40bd5a09
lhnaseeb63 <lhnaseeb@gmail.com>
2024-03-08 18:41:18 +0300

    deleted storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,11 +16,5 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
-  },
-  "storage": {
-    "imagestorage": {
-      "BucketName": "string",
-      "Region": "string"
-    }
   }
 }
